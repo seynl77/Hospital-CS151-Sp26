baf1a1a21330b48e0508828a58f9270899bf9b43
Derek <derekyeh2004@gmail.com>
2026-03-14 00:56:23 -0700

    added getters and setters for Person class

---
+++ Person.java
@@ -0,0 +1,48 @@
+public class Person {
+    private String name;
+    private int id;
+    private int age;
+    private String gender;
+    private int phoneNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+}
