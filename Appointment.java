7d0a80748b54808ebd60e4d2ece96c73670f2512
seynl77 <lixingyuan1998@gmail.com>
2026-03-17 11:11:08 -0700

    added appointment class

---
+++ Appointment.java
@@ -0,0 +1,26 @@
+public class Appointment {
+    private int appointmentId;
+    private Patient patient;
+    private Doctor doctor; //to be added
+    private String date;
+    private String time;
+    private String status;
+
+    public Appointment(int appointmentId, Patient patient, Doctor doctor, String date, String time) {
+        this.appointmentId = appointmentId;
+        this.patient = patient;
+        this.doctor = doctor;
+        this.date = date;
+        this.time = time;
+        this.status = "Scheduled";
+    }
+
+    public void displayInfo() {
+        System.out.println("Appointment ID: " + appointmentId);
+        System.out.println("Patient: " + patient.getName());
+        System.out.println("Doctor: " + doctor.getName());
+        System.out.println("Date: " + date);
+        System.out.println("Time: " + time);
+        System.out.println("Status: " + status);
+    }
+}
